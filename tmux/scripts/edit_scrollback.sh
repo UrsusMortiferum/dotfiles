639dafbb6eaf264b28aa638b4c75f2ba4b87ff31
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2024-06-02 14:47:03 +0200

    tmux - TODO + got rid of junk files

--- tmux/scripts/edit_scrollback.sh
+++ tmux/scripts/edit_scrollback.sh
@@ -6,6 +6,10 @@ tmpfile=$(mktemp /tmp/tmux-buffer-XXXXXX) || { echo "Failed to create temp file"
 tmux capture-pane -S -50000 -p > "$tmpfile" || { echo "Failed to capture pane"; exit 1; }
 tmux send-keys "$EDITOR $tmpfile" C-m || { echo "Failed to send keys"; rm -f "$tmpfile"; exit 1; }
 
+# TODO: Transform this into simple TMUX plugin
+# 	Include some sort of popup window (float) for quick editing
+# 	E.g.: cmd creation based on previous output
+
 # If you want to remove the file after editing
 # Put this autocmd in your nvim config
 # vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
