75d382b6efed7ba7f820eb2a3e9aa1c1308fd19a
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2024-06-03 13:03:38 +0200

    fish updates

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -4,3 +4,8 @@ abbr mv "mv -i"
 abbr mkdir "mkdir -p"
 abbr which "type -a"
 abbr ls "ls -lagh"
+abbr fvim "fzf --height 40% --layout=reverse --border\
+    --preview 'bat --style=numbers --color=always --line-range :500 {}'\
+    --bind 'enter:become($EDITOR {})'"
+abbr ff "fzf --height 40% --layout=reverse --border\
+    --preview 'bat --style=numbers --color=always --line-range :500 {}'"
--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,23 @@
 set -gx COLORTERM truecolor
 set -gx EDITOR nvim
 
+if test -d .git
+    set -gx FZF_DEFAULT_COMMAND "fd --type f --hidden --exclude .git"
+else
+    set -gx FZF_DEFAULT_COMMAND "fd --type f --hidden"
+end
+# set -gx FZF_DEFAULT_OPTS "--height 40% --layout=reverse --border\
+#     --preview 'bat --style=numbers --color=always --line-range :500 {}'"
+# --bind 'ctrl-c:execute-silent(echo {} | pbcopy)'
+
+function fs -d "Switch tmux session"
+    tmux list-sessions -F "#{session_name}" | fzf | read -l result; and tmux switch-client -t "$result"
+end
+
+starship init fish | source
+# Set up fzf key bindings
+fzf --fish | source
+
 fish_config theme choose tokyonight_night
 
 if status --is-interactive
@@ -15,7 +32,5 @@ if status --is-interactive
         end
     end
 end
-
-starship init fish | source
 
 set -U fish_greeting
