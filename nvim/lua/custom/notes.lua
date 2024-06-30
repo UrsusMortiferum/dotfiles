221aea3e07d64619c47e0c6c2bca5e673ba1b3be
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2024-06-30 02:13:02 +0200

    obsidian.nvim testing + config

--- nvim/lua/custom/notes.lua
+++ nvim/lua/custom/notes.lua
@@ -2,6 +2,30 @@ require("obsidian").setup({
   workspaces = {
     { name = "notes", path = "~/notes/" },
   },
+  notes_subdir = "$in",
+  new_notes_location = "notes_subdir",
   completion = { nvim_cmp = true, min_chars = 2 },
-  new_notes_location = "current_dir",
+  -- new_notes_location = "current_dir",
+  templates = {
+    folder = "~/notes/Templates/",
+    date_format = "%Y-%m-%d-%a",
+    time_format = "%H:%M",
+  },
+  note_id_func = function(title)
+    -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+    -- In this case a note with the title 'My new note' will be given an ID that looks
+    -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+    local suffix = ""
+    if title ~= nil then
+      -- If title is given, transform it into valid file name.
+      suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+    else
+      -- If title is nil, just add 4 random uppercase letters to the suffix.
+      for _ = 1, 4 do
+        suffix = suffix .. string.char(math.random(65, 90))
+      end
+    end
+    -- return tostring(os.time()) .. "-" .. suffix
+    return os.date("%Y-%m-%d-%a-%H-%M-%S") .. "_" .. suffix
+  end,
 })
--- nvim/lua/custom/plugins/notes.lua
+++ nvim/lua/custom/plugins/notes.lua
@@ -3,6 +3,13 @@ return {
     "epwalsh/obsidian.nvim",
     lazy = true,
     ft = "markdown",
+    cmd = {
+      "ObsidianNew",
+      "ObsidianSearch",
+      "ObsidianToday",
+      "ObsidianLink",
+      "ObsidianLinkNew",
+    },
     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
     -- event = {
     --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
@@ -13,5 +20,12 @@ return {
     config = function()
       require("custom.notes")
     end,
+
+    vim.keymap.set("n", "<leader>nn", "<cmd>ObsidianNew<cr>", { desc = "[N]ew [N]ote" }),
+    vim.keymap.set("n", "<leader>sn", "<cmd>ObsidianSearch<cr>", { desc = "[S]earch [N]otes" }),
+    vim.keymap.set("n", "<leader>dn", "<cmd>ObsidianToday<cr>", { desc = "[D]aily [N]ote" }),
+    vim.keymap.set("n", "<leader>nl", "<cmd>ObsidianLink<cr>", { desc = "[N]ew [L]ink" }),
+    vim.keymap.set("n", "<leader>ln", "<cmd>'<,'>ObsidianLinkNew<cr>", { desc = "[L]ink [N]ew - Creates New Note" }),
+    vim.keymap.set("n", "<leader>bl", "<cmd>ObsidianBacklinks<cr>", { desc = "View [B]ack[L]inks" }),
   },
 }
