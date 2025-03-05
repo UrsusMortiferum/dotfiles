b9fa28b91d2fe804e29cbc37dacbd1b217f3f93b
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2025-03-05 18:46:11 +0100

    clean-up of old files

--- nvim_old/after/ftplugin/sql.lua
+++
@@ -1,8 +0,0 @@
-vim.opt_local.commentstring = "-- %s"
-
-vim.keymap.set(
-  "v",
-  "<leader>b",
-  ":lua ListToSQLString()<CR>",
-  { noremap = true, silent = true, desc = "List to SQL string to simplify life" }
-)
