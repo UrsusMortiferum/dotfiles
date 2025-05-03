b7d87fd0e8ea92a5c09ec4da820a6f6cabc68e8b
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2025-05-03 22:19:48 +0200

    Testing stuff

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    -- event = "LazyFile"
+    -- event = "LazyFile",
     dependencies = {
       {
         "williamboman/mason.nvim",
@@ -9,7 +9,7 @@ return {
       },
       "williamboman/mason-lspconfig.nvim",
       "WhoIsSethDaniel/mason-tool-installer.nvim",
-      { "j-hui/fidget.nvim", opts = {} },
+      -- { "j-hui/fidget.nvim", opts = {} },
     },
     config = function()
       require "config.lsp"
