05dcaa1087f00eac213cfc09d2e0cbd462cf4023
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2025-12-12 21:13:55 +0100

    Update

--- nixos/overlays/default.nix
+++ nixos/overlays/default.nix
@@ -3,6 +3,6 @@
 {
   additions = final: _prev: import ../pkgs final.pkgs;
   modifications = final: prev: {
-    inherit (import ./bootdev-cli.nix final prev) bootdev-cli;
+    # inherit (import ./bootdev-cli.nix final prev) bootdev-cli;
   };
 }
